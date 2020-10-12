499fa68f55adf7a383b94442bb7cfe7a233daa86
David Olurebi <olurebidavid@gmail.com>
2020-10-12 17:12:20 +0400

    email content changed GS V-1

--- resources/views/emails/user/Verify.blade.php
+++ resources/views/emails/user/Verify.blade.php
@@ -4,7 +4,7 @@
 Thanks for registering with us, {{$userName}}. We assure you a sterling service. <br>
 In order to enjoy all of the many services we provide, please verify your email by clicking the button below.
 
-@component('mail::button', ['url' => $url, 'color'=>'success'])
+@component('mail::button', ['url' => $url, 'color'=>'dark'])
 Verify Email
 @endcomponent
 
