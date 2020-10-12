2691c2d5855de754d0c5c1d3d22e43a8e8d4d45f
David Olurebi <olurebidavid@gmail.com>
2020-10-12 17:25:59 +0400

    email content changed GS V-1

--- resources/views/emails/user/Verify.blade.php
+++ resources/views/emails/user/Verify.blade.php
@@ -10,5 +10,5 @@ Verify Email
 
 Thanks,<br>
 Glospace Logistics
-{<img src="{{asset('img/GS.png')}}" alt="Glospace" width="100">}
+<img src="{{asset('img/GS.png')}}" alt="Glospace" style="width: 100px">
 @endcomponent
